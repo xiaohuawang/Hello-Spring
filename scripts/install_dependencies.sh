478c72d4ea88402cf33f98731b14a58c80d7e6b7
“wxaohu” <wxaohu@amazon.com>
2018-09-20 11:08:20 -0700

    changed appspec and buildspec

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # check if mvn is installed
+
+echo '$PWD' > /var/log/install_dependencies.out 2>&1
 if type -p mvn;
 then
     echo "found maven executable in PATH"
@@ -17,7 +19,5 @@ else
     echo "can't find jdk, installing jdk..."
     sudo bash scripts/install_jdk.sh
 fi
-
-echo "install_dependencies end" > /var/log/install_dependencies.out 2>&1
 
 echo "end of install_dependencies..."
