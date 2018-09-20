bd11cb033f3d3057aee93cc418385143c2047777
“wxaohu” <wxaohu@amazon.com>
2018-09-20 16:23:56 -0700

    changed appspec and buildspec

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # go to the jar directory
-JAR_DIRECTORY=/var/www/html/result/
-cd $JAR_DIRECTORY
+# JAR_DIRECTORY=/var/www/
+# cd $JAR_DIRECTORY
 
 # use java to excute jar file
 java -jar my-app-1.0-SNAPSHOT.jar
