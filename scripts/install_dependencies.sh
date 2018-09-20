cf7e07749f70656c747adcb85e6ef338ddbe8754
“wxaohu” <wxaohu@amazon.com>
2018-09-20 11:00:15 -0700

    changed appspec and buildspec

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,7 +6,7 @@ then
     echo "found maven executable in PATH"
 else
     echo "can't find maven in PATH"
-    bash ./install_maven.sh
+    bash scripts/install_dependencies.sh
 fi
 
 # check if jdk is installed
@@ -15,7 +15,7 @@ then
     echo "found jdk"
 else
     echo "can't find jdk, installing jdk..."
-    sudo bash ./install_jdk.sh
+    sudo bash scripts/install_jdk.sh
 fi
 
 echo "install_dependencies end" > /var/log/install_dependencies.out 2>&1
