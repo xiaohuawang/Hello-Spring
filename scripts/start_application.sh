23f53fc351e41fb56e72a672764e4beb1e9f17b7
“wxaohu” <wxaohu@amazon.com>
2018-09-20 17:40:53 -0700

    Fix permissions and tweak startup and shutdown

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+kill $(cat /tmp/process.pid)
+[ec2-user@ip-172-31-42-122 tmp]$ cat /var/www/lol/start_application.sh
+#!/bin/bash
+
 # go to the jar directory
 # JAR_DIRECTORY=/var/www/
 # cd $JAR_DIRECTORY
 
 # use java to excute jar file
-java -jar demo-0.0.1-SNAPSHOT.jar
+java -jar /var/www/lol/demo-0.0.1-SNAPSHOT.jar > /tmp/output.log 2>&1 &
+
+echo $! > /tmp/process.pid
